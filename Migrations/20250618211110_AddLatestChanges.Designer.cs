e3562c740e2bb4344bbf5f6d4856cc7ff47bf6a4
Kaleem Shahzad <kaleemshahzad131@gmail.com>
2025-06-21 18:44:55 +0500

    User API Controlleer

--- Migrations/20250618211110_AddLatestChanges.Designer.cs
+++ Migrations/20250618211110_AddLatestChanges.Designer.cs
@@ -4,6 +4,7 @@ using EbayChatBot.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EbayChatBot.API.Migrations
 {
     [DbContext(typeof(EbayChatDbContext))]
-    partial class EbayChatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250618211110_AddLatestChanges")]
+    partial class AddLatestChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
